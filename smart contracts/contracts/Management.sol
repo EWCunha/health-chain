e65988e3f7b7fd4b27ae632b18f329e8890d68c8
Eduardo W. da Cunha <eduardowestc@gmail.com>
2022-11-12 19:16:02 -0300

    added commentaries

--- smart contracts/contracts/Management.sol
+++ smart contracts/contracts/Management.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 import "./MedicalHistory.sol";
 
 contract Management {
+    // --- STATE VARIABLES ---
     /**
     note
     0: CARDIO
@@ -19,15 +20,26 @@ contract Management {
     .
     */
 
+    /**
+    @notice mapping from patient to specialty contract (patient >> specialtyId >> contract address)
+    */
     mapping(address => mapping(uint256 => address)) public histories;
+    /**
+    @notice mapping of general trusted parties allowed to retrieve the medical history of all registered patients
+    */
     mapping(address => bool) public generalTrustedParty;
+    /**
+    @notice mapping of the managers addresses of this contract
+    */
     mapping(address => bool) public managers;
 
+    // --- EVENTS ---
     event UpdatedTrustedParty(address indexed party, bool status);
     event HistoryDeployed(
         address indexed history,
         address indexed owner,
-        uint256 specialtyId
+        uint256 specialtyId,
+        string uri
     );
     event ManagerUpdated(
         address indexed manager,
@@ -35,11 +47,21 @@ contract Management {
         bool status
     );
 
+    /**
+    @notice only allowed manager address
+    */
     modifier onlyAllowed() {
         require(managers[msg.sender], "Not allowed");
         _;
     }
 
+    // --- FUNCTIONS ---
+    /**
+    @notice updateTrustedParty: updates the trusted parties mapping
+    only allowed managers can call this function
+    @param _party: party address
+    @param _status: status to be updated
+    */
     function updateTrustedParty(address _party, bool _status)
         external
         onlyAllowed
@@ -50,22 +72,33 @@ contract Management {
         emit UpdatedTrustedParty(_party, _status);
     }
 
-    function deployHistory(string memory uri, uint256 _specialtyId) external {
+    /**
+    @notice deployHistory: deploys new contract for the medical history of the given specialty. Each address can deploy
+    only one contract for each medical specialty
+    @param _uri: medical history IPFS URI
+    @param _specialtyId: ID of the medical hisotry specialty
+    */
+    function deployHistory(string memory _uri, uint256 _specialtyId) external {
         require(
             histories[msg.sender][_specialtyId] == address(0),
             "Already deployed"
         );
 
         MedicalHistory history = new MedicalHistory(
-            uri,
+            _uri,
             msg.sender,
             _specialtyId
         );
         histories[msg.sender][_specialtyId] = address(history);
 
-        emit HistoryDeployed(address(history), msg.sender, _specialtyId);
+        emit HistoryDeployed(address(history), msg.sender, _specialtyId, _uri);
     }
 
+    /**
+    @notice updateManager: updates a manager status. Only managers are allowed to call this function.
+    @param _manager: the manager's address
+    @param _status: the new manager's status
+    */
     function updateManager(address _manager, bool _status)
         external
         onlyAllowed
--- smart contracts/contracts/MedicalHistory.sol
+++ smart contracts/contracts/MedicalHistory.sol
@@ -4,16 +4,27 @@ pragma solidity ^0.8.9;
 import "./interfaces/IManagement.sol";
 
 contract MedicalHistory {
-    address public immutable owner;
-    IManagment immutable management;
-    bytes32 public uri;
-    bool public allowAll;
-    bool public considerTrustedParties;
-    mapping(address => bytes32) public allowedParties;
-    uint256 public immutable specialtyId;
+    // --- STATE VARIABLES ---
+    address public immutable owner; // owber's/patient's address.
+    IManagment immutable management; // management contract pointer.
+    bytes32 public uri; // medical history IPFS URI.
+    bool public allowAll; // allows anyone to see the IPFS URI.
+    bool public considerTrustedParties; // consider the management contract list of trusted parties.
+    mapping(address => bytes32) public allowedParties; // mapping from parties addresses to encrypted IPFS URIs.
+    uint256 public immutable specialtyId; // specialty ID of this medical history contract.
 
+    // --- EVENTS ---
     event UpdatedURI(address indexed responsible, string uri);
     event UpdatedParty(address indexed party);
+
+    // --- MODIFIERS ---
+    /**
+    @notice only owner modifier.
+    */
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner allowed");
+        _;
+    }
 
     constructor(
         string memory _uri,
@@ -26,18 +37,21 @@ contract MedicalHistory {
         specialtyId = _specialtyId;
     }
 
+    // --- EXTERNAL FUNCTIONS ---
+    /**
+    @notice getURI: returns the IPFS URI of the medical history for the owner or allowed parties.
+    */
     function getURI() external view returns (string memory) {
-        if (
-            msg.sender == owner ||
-            allowAll ||
-            management.generalTrustedParty(msg.sender)
-        ) {
+        if (msg.sender == owner || allowAll || _getGeneralTrustedParty()) {
             return _bytes32ToString(uri);
         } else {
             return _bytes32ToString(allowedParties[msg.sender]);
         }
     }
 
+    /**
+    @notice updateURI: updates the IPFS URI of the medical history.
+    */
     function updateURI(string memory _newURI) external {
         if (msg.sender == owner) {
             uri = _stringToBytes32(_newURI);
@@ -50,13 +64,54 @@ contract MedicalHistory {
         emit UpdatedURI(msg.sender, _newURI);
     }
 
-    function updateAllowedParty(string memory _uri, address _party) external {
-        require(msg.sender == owner, "Not allowed");
+    /**
+    @notice updateAllowedParty: updates party URI. To not allow party anymore, set _uri to empty string.
+    @param _uri: new IPFS URI.
+    @param _party: party address.
+    */
+    function updateAllowedParty(string memory _uri, address _party)
+        external
+        onlyOwner
+    {
         allowedParties[_party] = _stringToBytes32(_uri);
 
         emit UpdatedParty(_party);
     }
 
+    /**
+    @notice setAllowAll: updates allowAll variable. Only the owner is allowed.
+    @param _allowAll: new boolean value for allowAll.
+    */
+    function setAllowAll(bool _allowAll) external onlyOwner {
+        allowAll = _allowAll;
+    }
+
+    /**
+    @notice setConsiderTrustedParties: updates considerTrustedParties variable. Only the owner is allowed.
+    @param _considerTrustedParties: new boolean value for considerTrustedParties.
+    */
+    function setConsiderTrustedParties(bool _considerTrustedParties)
+        external
+        onlyOwner
+    {
+        considerTrustedParties = _considerTrustedParties;
+    }
+
+    // --- INTERNAL FUNCTIONS ---
+    /**
+    @notice _getGeneralTrustedParty: returns if the msg.sender is a trusted party from the management contract.
+    */
+    function _getGeneralTrustedParty() internal view returns (bool) {
+        return
+            considerTrustedParties
+                ? management.generalTrustedParty(msg.sender)
+                : false;
+    }
+
+    /**
+    @notice _bytes32ToString: converts bytes32 to string.
+    @param _bytes32: bytes32 value to be converted to string.
+    */
     function _bytes32ToString(bytes32 _bytes32)
         internal
         pure
@@ -73,6 +128,10 @@ contract MedicalHistory {
         return string(bytesArray);
     }
 
+    /**
+    @notice _stringToBytes32: converts string to bytes32.
+    @param source: string value to be converted to bytes32.
+    */
     function _stringToBytes32(string memory source)
         internal
         pure
